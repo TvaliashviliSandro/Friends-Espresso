1d506b355df5dd39f98526daac331bfa1942c2cb
Lmosakhlishvili <122836477+Lmosakhlishvili@users.noreply.github.com>
2025-02-06 02:53:10 +0400

    Update UIElementPage.kt

--- sample-app/src/androidTest/java/com/atiurin/sampleapp/pages/UIElementPage.kt
+++ sample-app/src/androidTest/java/com/atiurin/sampleapp/pages/UIElementPage.kt
@@ -10,12 +10,4 @@ import org.hamcrest.Matchers.containsString
 import org.hamcrest.Matchers.instanceOf
 
 object UIElementPage {
-
-    val radioBtnInvisible: Matcher<View> by lazy { withId(R.id.invisible) }
-    val radioBtnVisible: Matcher<View> by lazy { withText("VISIBLE") }
-    val mainManuBtn: Matcher<View> by lazy { withClassName(CoreMatchers.`is`("android.widget.ImageButton")) }
-    val nameChandler: Matcher<View> by lazy { Matchers.allOf(withId(R.id.tv_name), withText("Chandler Bing")) }
-    val messageInputText: Matcher<View> by lazy { Matchers.allOf(withId(R.id.message_input_text), withText("Enter text")) }
-    val textInput: Matcher<View> by lazy { Matchers.allOf(withId(R.id.message_input_text)) }
-    val clearTextOption: Matcher<View> by lazy { withClassName(CoreMatchers.`is`("android.widget.ImageView")) }
 }
